10c894ca96aac509c4c068538f3aaf3828b3b353
Your Name <matteo.canavicci@hpe.com>
2025-04-04 16:51:53 +0200

    logger implementation

--- SQLTool.py
+++ SQLTool.py
@@ -13,7 +13,7 @@ class SQLTool(Tool):
         string: Query results as a string.
     """
 
-    def __init__(self, db, user, password=None, port=5432, host="localhost",table_name=None, name=None, description=None, inputs=None, output_type=None):
+    def __init__(self, db, user, password=None, port=5432, host="localhost",table_name=None, name=None, description=None, inputs=None, output_type=None, logger=None):
         super().__init__()
         self.pg_conn_dict = {
             'dbname': db,
@@ -23,6 +23,7 @@ class SQLTool(Tool):
             'host': host
         }
         self.name = name
+        self.logger = logger
         self.description = f"{description}\n{self.get_db_schema(table_name)}"
         self.inputs = inputs
         self.output_type = output_type
@@ -32,10 +33,8 @@ class SQLTool(Tool):
             conn = psycopg2.connect(**self.pg_conn_dict)
             cursor = conn.cursor()
         except OperationalError as e:
-            print("Failed to connect to database")
-            print(f"Error: {e}")
+            self.logger.error("Failed to connect to database", exc_info=True)
             return None
-
         query = f"""
                 SELECT column_name, data_type, is_nullable, column_default
                 FROM information_schema.columns
@@ -45,33 +44,40 @@ class SQLTool(Tool):
             cursor.execute(query)
             columns = cursor.fetchall()
             conn.commit()
+            schema_lines = [f" - column name: {col[0]}, data type: {col[1]}, is nullable: {col[2]}" for col in columns]
+            return f"Database Schema for '{table_name}':\n" + "\n".join(schema_lines)
         except Exception as e:
-            # TODO Implement logging for error handling
-            columns = f"SQL Execution Error: {e}"
-            return ""
+            self.logger.error("SQL Execution Error", exc_info=True)
+            return None
         finally:
-            conn.close()
-        schema_lines = [f" - column name: {col[0]}, data type: {col[1]}, is nullable: {col[2]}" for col in columns]
-        return f"Database Schema for '{table_name}':\n" + "\n".join(schema_lines)
+            if conn:
+                conn.close()
+        
 
     def is_safe_sql(self, sql: str) -> bool:
         """Only allow SELECT queries (basic safeguard)."""
-        safe_keywords = ["select", "with"]  # Add CTE support
+        safe_keywords = ["select", "with"] 
         sql_lower = sql.strip().lower()
         return any(sql_lower.startswith(kw) for kw in safe_keywords)
     
     def forward(self, query: str):
         if not self.is_safe_sql(query):
-            return "❌ Unsafe SQL query detected. Only SELECT queries are allowed."
-        
-        conn = psycopg2.connect(**self.pg_conn_dict)
-        cursor = conn.cursor()
+            self.logger.error("❌ Unsafe SQL query detected. Only SELECT queries are allowed.")
+            return None
+        try:
+            conn = psycopg2.connect(**self.pg_conn_dict)
+            cursor = conn.cursor()
+        except OperationalError as e:
+            self.logger.error("Failed to connect to database", exc_info=True)
+            return None
         try:
             cursor.execute(query)
             results = cursor.fetchall()
             conn.commit()
         except Exception as e:
-            results = f"SQL Execution Error: {e}"
+            self.logger.error("SQL Execution Error", exc_info=True)
+            return None
         finally:
-            conn.close()
+            if conn:
+                conn.close()
         return str(results)
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import os
+import logging
+import sys
 from SQLTool import SQLTool
 from dotenv import load_dotenv
 from smolagents import LiteLLMModel, GradioUI, CodeAgent
 
 if __name__ == "__main__":
+    logger = logging.getLogger("agent_logger")
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('[%(asctime)s] %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    if not logger.handlers:
+        logger.addHandler(handler)
+
     load_dotenv()
 
     model = LiteLLMModel(
@@ -14,8 +28,7 @@ if __name__ == "__main__":
         max_tokens=6000
     )
 
-    tool_description =  f"""
-    A tool designed to execute SQL queries on a structured database and return the results as a string. 
+    tool_description =  f"""A tool designed to execute SQL queries on a structured database and return the results as a string. 
     It supports SELECT queries for data retrieval, including filtering, aggregation, and joins. 
     The tool processes queries efficiently and returns results in a human-readable string format. 
     Ensure queries are well-formed to prevent errors. Avoid SQL injection by properly handling input parameters.
@@ -40,9 +53,12 @@ if __name__ == "__main__":
         name="sql_executor",
         description=tool_description,
         inputs=tool_inputs,
-        output_type=tool_output_type
+        output_type=tool_output_type,
+        logger=logger
     )
-    print(sql_tool.description)
+
+    logger.info(f"The description of the toolw is the following:\n{sql_tool.description}")
+
     agent = CodeAgent(
         tools=[sql_tool],
         model=model
